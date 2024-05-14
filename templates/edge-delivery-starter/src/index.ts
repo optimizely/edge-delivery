870f59a7302acef222b40ec6d27495570a0cb517
Micah Heineck <94473452+micahheineckoptimizely@users.noreply.github.com>
2024-05-14 14:53:42 -0400

    [CJS-7861] Updates to enable the minimal starter template  (#2)
    * Update README and lib version, for the minimal starter template

--- templates/edge-delivery-starter/src/index.ts
+++ templates/edge-delivery-starter/src/index.ts
@@ -25,43 +25,15 @@ export default {
 };
 
 async function handleRequest(request: Request, env: Env, ctx: ExecutionContext) {
-    let requestUrl = new URL(request.url);
+    // Configure all the options to pass to Optimizely
+    const options = {
+        "snippetId": env.SNIPPET_ID,
+        "devUrl": env.DEV_URL,
+    };
 
-    let control;
-    let targetUrl;
-    let targetRequest : Request;
-
-    // TODO: Move this logic to the web sdk with a configuration option 
-    if (requestUrl.host.includes("localhost") || 
-            (requestUrl.host.startsWith('edge-delivery-starter') && requestUrl.host.endsWith('workers.dev'))) {
-        targetUrl = env.DEV_URL;
-        targetRequest = new Request(targetUrl, request);
-        control = await fetch(targetUrl, targetRequest);
-    } else {
-        targetUrl = requestUrl;
-        targetRequest = request;
-        control = await fetch(request);
-    }
-
-    // TODO: Remove try/catch and put safeguards in applyExperiments
-    try {
-        // Configure all the options to pass to Optimizely
-        const options = {
-            "snippetId": env.SNIPPET_ID,
-        };
-
-        // Make experiment decisions based on the request information
-        // Apply those changes to the control
-        // Any decisions or changes that cannot be made here are packaged together 
-        // and added to the <head> element for execution on the browser 
-        const treatedControl = await applyExperiments(targetRequest, control.clone(), ctx, options);
-
-        return treatedControl;
-
-    } catch (e) {
-        // If an error occurs, or if a timeout occurs, etc., bail and don't apply experiments
-        console.log("An error occurred, returning Control instead", e);
-    }
-
-    return control;
+    // Make experiment decisions based on the request information
+    // Apply those changes to the control
+    // Any decisions or changes that cannot be made here are packaged together 
+    // and added to the <head> element for execution on the browser 
+    return await applyExperiments(request, ctx, options);
 }
